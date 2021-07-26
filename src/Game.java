b404b4ba9c6ebffb2a0e53d2a302ee0756b522af
AmgadElsayed2021 <78882999+AmgadElsayed2021@users.noreply.github.com>
2021-07-26 18:38:09 -0230

    Update Game.java
    re edit the whole game and start again due too bugs

--- src/Game.java
+++ src/Game.java
@@ -1,90 +1,31 @@
 
-//Importing the java package
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Random;
-import java.util.LinkedList;
-import java.net.*;
-import java.io.*;
 
-//Main class of the program
-class Game{
-    //Printing the user's guessed character
-    public static void printArray(char[] array){
-        for(int i =0; i < array.length; i++){
-            System.out.print(array[i] + " ");
+public class Game {
+    public static void main(String[] args) throws Exception{
+    // grab a text file
+        File sourceFile= new File("src/usa.txt");
+        Scanner sc =new Scanner(sourceFile);
+        Scanner inputDevice =new Scanner(System.in);
+        ArrayList <String> words =new ArrayList<>();
+        while(sc.hasNextLine()){
+            words.add(sc.nextLine());
         }
-        System.out.println();
-    }
+        //get a random word from the file and assign it to an array to split into chars .
+        String puzzleWord= words.get((int)(Math.random()*words.size()));
+        char[] wordArray = puzzleWord.toCharArray();
+        char[] playerGuesses = new char[wordArray.length];
 
-    public static boolean isTheWordGuessed(char[] array){
-        for(int i =0; i < array.length; i++){
-            if(array[i] == '_') {return false;}
+        for (int i =0;i<wordArray.length;i++){
+            playerGuesses[i] = '?';
         }
-        return true;
-    }
+        System.out.println("*********************************");
 
-    //Main Class of the program
-    public static void main(String[] args){
-        //Creating the object of scanner class
-        Scanner scanner = new Scanner(System.in);
-        //Creating the object of random class
-        Random random = new Random();
-        //Array of available strings
-        String[] guesses = {"stechies","tutorial","programming","google","facebook","sap","tomato"};
-
-        boolean weArePlaying = true;
-        //Checking with user that Are we still in the play
-        while(weArePlaying){
-            System.out.println("Welcome to my game of Hangman");
-            char[] randomWordToGuess = guesses[random.nextInt(guesses.length)].toCharArray();
-
-            int NumberOfGuesses = randomWordToGuess.length;
-            char[] playerGuess = new char[NumberOfGuesses];
-
-            for(int i = 0; i < playerGuess.length; i++){
-                playerGuess[i] = '_';
-            }
-
-            boolean wordIsGuessed = false;
-            int tries = 0;
-            //Loop will till the number of guesses will not
-            // equals to the available amount of guesses
-            while( !wordIsGuessed && tries != NumberOfGuesses ){
-                System.out.print("Current guesses : ");
-                printArray(playerGuess);
-                //Giving information to the user how many guesses are available
-                System.out.printf("You have %d tries left.n", NumberOfGuesses - tries);
-                //Telling user what to enter
-                System.out.println("Enter a single character : ");
-                char input = scanner.nextLine().charAt(0);
-                //Increasing the tries counter
-                tries++;
-
-                if(input == '-'){
-                    weArePlaying = false;
-                    wordIsGuessed = true;
-                }else{
-
-                    for(int i = 0; i < randomWordToGuess.length; i++){
-                        if(randomWordToGuess[i] == input) {
-                            playerGuess[i] = input;
-                        }
-                    }
-
-                    if(isTheWordGuessed(playerGuess)){
-                        wordIsGuessed = true;
-                        System.out.println("nn**************************nCongratulations you won!n**************************");
-                    }
-
-                }
-            }
-            //If loop if user out of moves
-            if(!wordIsGuessed) System.out.println("You ran out of guesses :/");
-            //Asking user if user wants to continue the game
-            System.out.print("nDo you want to play another game? (y/n) : ");
-            String anotherGame = scanner.nextLine();
-            if(anotherGame.equals("n")) weArePlaying = false;
-        }
-        System.out.println("Game Over");
-    }
-}
+        // limit the user guesses
+        int guesses =7;
+        drawHangman(guesses);
+        // create a while loop to enter the guesses
+        boolean finish =false;
+    }}
